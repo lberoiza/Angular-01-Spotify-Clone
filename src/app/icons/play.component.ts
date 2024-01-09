c853955132603974767680cbc965ea14669ba012
Luis B <luis.beroiza@adesso.de>
2024-01-09 20:31:28 +0100

    Style Player-Track-Control
    center all elements

--- src/app/icons/play.component.ts
+++ src/app/icons/play.component.ts
@@ -5,9 +5,9 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <svg [class]="class" viewBox="0 0 24 24" fill="currentColor"
-    ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg
-    >
+    <svg [class]="class" viewBox="0 0 24 24" fill="currentColor">
+      <path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path>
+    </svg>
   `,
   styles: ``
 })
