b587c4c4a55df819e4d76231a6f8db7b3343b11f
Luis B <luis.beroiza@adesso.de>
2024-01-10 14:15:56 +0100

    Add RouterOutlet
    Placeholder to show elements with the routerlink

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {PlayerComponent} from "../player/player.component";
 import {SearchPlaylistComponent} from "../search-playlist/search-playlist.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'main-component',
   standalone: true,
   imports: [
     SearchPlaylistComponent,
-    PlayerComponent
+    PlayerComponent,
+    RouterOutlet
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
