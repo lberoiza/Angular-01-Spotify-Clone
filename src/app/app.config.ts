29855620c5fa53856d163ec843f7b1e4c112fce4
Luis B <luis.beroiza@adesso.de>
2024-01-16 14:25:14 +0100

    Add Store Module for Angular
    Store: ng add @ngrx/store@latest
    Store Development Tools: ng add @ngrx/store-devtools@latest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      withViewTransitions()
-    ),
-    provideAnimations()
-  ]
+    provideRouter(routes, withViewTransitions()),
+    provideAnimations(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
