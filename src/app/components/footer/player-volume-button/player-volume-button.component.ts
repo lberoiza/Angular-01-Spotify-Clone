35d0a683209007944b7381114cd287aeceb78734
Luis B <luis.beroiza@adesso.de>
2024-01-18 21:08:03 +0100

    Use the AppState to control the volumen of the app
    add feature volume control and mute using the State

--- src/app/components/footer/player-volume-button/player-volume-button.component.ts
+++ src/app/components/footer/player-volume-button/player-volume-button.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VolumeSilencedComponent } from "@/icons/volume-silenced.component";
 import { VolumeLowComponent } from "@/icons/volume-low.component";
 import { VolumeMediumComponent } from "@/icons/volume-medium.component";
 import { VolumeFullComponent } from "@/icons/volume-full.component";
+import { AppState } from "@/store/app.state";
+import { Store } from "@ngrx/store";
+import { SelectPlayerVolume } from "@/store/player-store/playerstore.selectors";
+import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
 
 @Component({
   selector: 'app-player-volume-button',
@@ -16,12 +20,39 @@ import { VolumeFullComponent } from "@/icons/volume-full.component";
   templateUrl: './player-volume-button.component.html',
   styleUrl: './player-volume-button.component.css'
 })
-export class PlayerVolumeButtonComponent {
+export class PlayerVolumeButtonComponent implements OnInit {
 
-  protected currentVolume: number = 0.1;
+  protected currentVolume: number = 0;
+  protected previousVolume: number = 0;
 
-  isVolumeLow = () => this.currentVolume > 0 && this.currentVolume < 0.5
-  isVolumeMedium = () => this.currentVolume >= 0.5 && this.currentVolume < 0.9
-  isVolumeFull = () => this.currentVolume >= 0.9
+
+  constructor(private store: Store<AppState>) {
+  }
+
+
+  ngOnInit() {
+    this.store.select(SelectPlayerVolume)
+      .subscribe((volume: number) => {
+        this.currentVolume = volume
+      });
+  }
+
+  protected isVolumeSilenced = () => this.currentVolume == 0;
+  protected isVolumeLow = () => this.currentVolume > 0 && this.currentVolume < 0.5;
+  protected isVolumeMedium = () => this.currentVolume >= 0.5 && this.currentVolume < 0.9;
+  protected isVolumeFull = () => this.currentVolume >= 0.9;
+
+  protected volumeButtonClicked() {
+    if (this.isVolumeSilenced()) {
+      this.updateVolumeOnStore(this.previousVolume);
+    } else {
+      this.previousVolume = this.currentVolume;
+      this.updateVolumeOnStore(0);
+    }
+  }
+
+  private updateVolumeOnStore(volume: number) {
+    this.store.dispatch(PlayerStoreActions.setVolume({volume}));
+  }
 
 }
--- src/app/components/footer/player-volume-control/player-volume-control.component.ts
+++ src/app/components/footer/player-volume-control/player-volume-control.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSliderModule } from "@angular/material/slider";
 import { FormsModule } from '@angular/forms';
 import { PlayerVolumeButtonComponent } from "@/components/footer/player-volume-button/player-volume-button.component";
+import { Store } from "@ngrx/store";
+import { AppState } from "@/store/app.state";
+import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
+import { SelectPlayerVolume } from "@/store/player-store/playerstore.selectors";
 
 @Component({
   selector: 'player-volume-control',
@@ -14,10 +18,24 @@ import { PlayerVolumeButtonComponent } from "@/components/footer/player-volume-b
   templateUrl: './player-volume-control.component.html',
   styleUrl: './player-volume-control.component.css'
 })
-export class PlayerVolumeControlComponent {
+export class PlayerVolumeControlComponent implements OnInit{
 
   protected maxVolume: number = 1;
   protected volumeStep: number = 0.01;
   protected currentVolume: number = 0.1;
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit() {
+    this.store.select(SelectPlayerVolume)
+      .subscribe((volume: number) => {
+        this.currentVolume = volume
+      });
+  }
+
+  updateCurrentVolumeOnStore() {
+    this.store.dispatch(PlayerStoreActions.setVolume({volume: this.currentVolume}));
+  }
 
 }
