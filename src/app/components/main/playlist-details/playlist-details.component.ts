73644ccfbf33692b4ffe5edeac234332ba397bb7
Luis B <luis.beroiza@adesso.de>
2024-01-22 16:27:21 +0100

    Save in the store the information of the selected playlist

--- src/app/components/main/playlist-details/playlist-details.component.ts
+++ src/app/components/main/playlist-details/playlist-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
 import type { Playlist, Song } from "@/data/data";
-import { PlaylistButtonPlayComponent } from "@/components/main/playlist-button-play/playlist-button-play.component";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AppState } from "@/store/app.state";
+import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
 import { PlaylistApiService } from "@/services/playlist-api.service";
-import {
-  PlaylistDetailsMusictableComponent
-} from "../playlist-details-musictable/playlist-details-musictable.component";
+import { PlaylistButtonPlayComponent } from "@/components/main/playlist-button-play/playlist-button-play.component";
+import { PlaylistDetailsMusictableComponent } from "@/components/main/playlist-details-musictable/playlist-details-musictable.component";
+import { Store } from "@ngrx/store";
 
 export interface SongDuration {
   hours: number;
@@ -36,6 +37,7 @@ export class PlaylistDetailsComponent implements OnInit {
   }
 
   constructor(
+    private store: Store<AppState>,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private playlistApiService: PlaylistApiService) {
@@ -46,11 +48,23 @@ export class PlaylistDetailsComponent implements OnInit {
     this.activatedRoute.params.subscribe(params => {
       let id = params['id'];
       if (id) {
-        this.playlistDetails = this.playlistApiService.getPlaylistById(id);
-        this.playlistSongs = this.playlistApiService.getSongsByPlaylist(this.playlistDetails);
-        this.playlistDuration = this.getTotalTime();
+        this.getPlayListDetailsAndSongsFromApi(id);
+        this.updateCurrentPlaylistStore();
       }
     });
+  }
+
+  getPlayListDetailsAndSongsFromApi(playlistId: string) {
+    this.playlistDetails = this.playlistApiService.getPlaylistById(playlistId);
+    this.playlistSongs = this.playlistApiService.getSongsByPlaylist(this.playlistDetails);
+    this.playlistDuration = this.getTotalTime();
+  }
+
+  private updateCurrentPlaylistStore() {
+    this.store.dispatch(PlayerStoreActions.setCurrentPlaylistSongs({songs: this.playlistSongs}));
+    if(this.playlistDetails) {
+      this.store.dispatch(PlayerStoreActions.setCurrentPlaylist({playlist: this.playlistDetails}));
+    }
   }
 
 
