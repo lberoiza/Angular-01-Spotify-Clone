4ffc15669adca5e42d41a8028f37203de40f9e67
Luis B <luis.beroiza@adesso.de>
2024-01-23 15:05:51 +0100

    Remove Example Code from index
    this example was to interact with the state

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,35 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Playlist, playlists } from "@/data/data";
 import { PlaylistCardComponent } from "@/components/main/playlist-card/playlist-card.component";
-import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
-import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
-import { SelectPlayerIsPlaying } from "@/store/player-store/playerstore.selectors";
-import { AppState } from "@/store/app.state";
-import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: 'index',
   standalone: true,
-  imports: [PlaylistCardComponent, AsyncPipe],
+  imports: [PlaylistCardComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent {
   @Input() playlists: Playlist[] = playlists
-
-  protected isPlaying$: Observable<boolean> = new Observable<boolean>();
-
-  constructor(
-    private store: Store<AppState>
-  ) {
-  }
-
-
-  ngOnInit() {
-    console.log('Initialize index page')
-    this.isPlaying$ = this.store.select(SelectPlayerIsPlaying);
-    this.store.dispatch(PlayerStoreActions.setIsPlaying({ isPlaying: false }));
-  }
-
 }
