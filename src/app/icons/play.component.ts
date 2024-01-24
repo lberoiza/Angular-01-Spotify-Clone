484af2cf58d9368474d33b6cc37823fb2304c0e6
Luis B <luis.beroiza@adesso.de>
2024-01-24 16:33:56 +0100

    Fix Play and Pause Button of Player
    The size of the symbols was not right

--- src/app/icons/play.component.ts
+++ src/app/icons/play.component.ts
@@ -5,8 +5,9 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <svg [class]="class" viewBox="0 0 24 24" fill="currentColor">
-      <path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path>
+    <svg [class]="class" viewBox="0 0 16 16" role="img" height="16" width="16" >
+      <path
+        d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
     </svg>
   `,
   styles: ``
