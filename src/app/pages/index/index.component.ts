e0312d63653e20d43a3f41d0427f603d1ee8da79
Luis B <luis.beroiza@adesso.de>
2024-01-24 16:11:29 +0100

    Add Greeting Component to the Index component

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { GreetingsUserComponent } from "@/components/main/greetings-user/greetings-user.component";
 import { Playlist, playlists } from "@/data/data";
 import { PlaylistCardComponent } from "@/components/main/playlist-card/playlist-card.component";
 
 @Component({
   selector: 'index',
   standalone: true,
-  imports: [PlaylistCardComponent],
+  imports: [
+    GreetingsUserComponent,
+    PlaylistCardComponent
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
