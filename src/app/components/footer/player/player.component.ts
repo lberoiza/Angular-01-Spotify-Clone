de6422a5bdbf466db197b2ad5eb351635005db49
Luis B <luis.beroiza@adesso.de>
2024-01-24 16:35:28 +0100

    Feature Play/Pause Icon on Player
    The Symbol changes if the Player are playing a song.

--- src/app/components/footer/player/player.component.ts
+++ src/app/components/footer/player/player.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { PlayerTrackControlComponent } from "@/components/footer/player-track-control/player-track-control.component";
-import { PlayComponent } from "@/icons/play.component";
+import { Component, OnInit } from '@angular/core';
+import type { AppState } from "@/store/app.state";
 import { PauseComponent } from "@/icons/pause.component";
+import { PlayComponent } from "@/icons/play.component";
 import { PlayerCurrentSongComponent } from "@/components/footer/player-current-song/player-current-song.component";
+import { PlayerTrackControlComponent } from "@/components/footer/player-track-control/player-track-control.component";
 import { PlayerVolumeControlComponent } from "@/components/footer/player-volume-control/player-volume-control.component";
+import { SelectPlayerIsPlaying } from "@/store/player-store/playerstore.selectors";
+import { Store } from "@ngrx/store";
 
 
 @Component({
@@ -19,6 +22,19 @@ import { PlayerVolumeControlComponent } from "@/components/footer/player-volume-
   templateUrl: './player.component.html',
   styleUrl: './player.component.css'
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit{
+
+  protected isPlaying: boolean = false;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.store.select(SelectPlayerIsPlaying).subscribe((isPlaying) => this.isPlaying = isPlaying);
+  }
+
+
 
 }
