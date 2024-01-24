8eea154ad42803baaf24cfe819c9a0928014c1fe
Luis B <luis.beroiza@adesso.de>
2024-01-24 15:09:45 +0100

    Refactor playlist-details.component to get the Information of the Playlist
    The information will be read from Store (CurrentPlaylist),
    if the Current Playlist is not in the Store will be gettet from API

--- src/app/components/main/playlist-details/playlist-details.component.ts
+++ src/app/components/main/playlist-details/playlist-details.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import type { Playlist, Song } from "@/data/data";
-import { ActivatedRoute, Router } from "@angular/router";
+import type { PlaylistDuration } from "@/libs/playlist-duration";
+import { ActivatedRoute } from "@angular/router";
 import { AppState } from "@/store/app.state";
-import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
 import { PlaylistApiService } from "@/services/playlist-api.service";
 import { PlaylistButtonPlayComponent } from "@/components/main/playlist-button-play/playlist-button-play.component";
+import { Store } from "@ngrx/store";
+import { getPlaylistDuration } from "@/libs/playlist-duration";
+import { take } from "rxjs";
 import {
   PlaylistDetailsMusictableComponent
 } from "@/components/main/playlist-details-musictable/playlist-details-musictable.component";
-import { StateManagerService } from "@/services/state-manager.service";
-import { Store } from "@ngrx/store";
-import { playlists } from "@/data/data";
+import {
+  SelectPlayerCurrentMusic,
+  SelectPlayerCurrentPlaylist,
+} from "@/store/player-store/playerstore.selectors";
 
-export interface SongDuration {
-  hours: number;
-  minutes: number;
-  seconds: number;
-}
 
 @Component({
   selector: 'app-playlist-details',
@@ -29,13 +28,13 @@ export interface SongDuration {
   styleUrl: './playlist-details.component.css',
   providers: [
     PlaylistApiService,
-    StateManagerService
   ]
 })
 export class PlaylistDetailsComponent implements OnInit {
   protected playlistDetails: Playlist | undefined = undefined;
   protected playlistSongs: Song[] = [];
-  protected playlistDuration: SongDuration = {
+  protected currentSong: Song | undefined = undefined;
+  protected playlistDuration: PlaylistDuration = {
     hours: 0,
     minutes: 0,
     seconds: 0
@@ -43,38 +42,43 @@ export class PlaylistDetailsComponent implements OnInit {
 
   constructor(
     private store: Store<AppState>,
-    private stateManagerService: StateManagerService,
-    private router: Router,
     private activatedRoute: ActivatedRoute,
     private playlistApiService: PlaylistApiService) {
   }
 
-
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
-      let id = params['id'];
-      if (id) {
-        ({
-          playlistDetails: this.playlistDetails,
-          playlistSongs: this.playlistSongs
-        } = this.stateManagerService.updatePlaylistDetailsAndSongsByPlaylistId(this.playlistApiService, this.store, id));
-      }
+      const paramPlaylistId = params['id'];
+      this.setPlaylistDetailsByPlaylistId(paramPlaylistId);
     });
   }
 
-  private getTotalTime(): SongDuration {
-    let totalOfSeconds = 0;
-
-    this.playlistSongs.forEach((song) => {
-      const [seconds = 0, minutes = 0, hours = 0] = song.duration.split(':').reverse().map(Number);
-      totalOfSeconds += hours * 3600 + minutes * 60 + seconds;
-    });
-
-    return {
-      hours: Math.floor(totalOfSeconds / 3600),
-      minutes: Math.floor((totalOfSeconds % 3600) / 60),
-      seconds: totalOfSeconds % 60
-    };
+  private setPlaylistDetailsByPlaylistId(paramPlaylistId: string): void {
+    this.store.select(SelectPlayerCurrentPlaylist)
+      .subscribe(playlistDetailsOnStore => {
+        if (playlistDetailsOnStore?.id === paramPlaylistId) {
+          this.getCurrentMusicFromStore();
+        } else {
+          this.getPlaylistDetailsAndSongFromApi(paramPlaylistId);
+        }
+      });
   }
 
+  private getCurrentMusicFromStore() {
+    this.store.select(SelectPlayerCurrentMusic)
+      .pipe(take(1))
+      .subscribe((currentMusic) => {
+        this.playlistDetails = currentMusic.playlist;
+        this.playlistSongs = currentMusic.songs;
+        this.currentSong = currentMusic.song;
+        this.playlistDuration = getPlaylistDuration(this.playlistSongs);
+      });
+  }
+
+  private getPlaylistDetailsAndSongFromApi(playlistId: string) {
+    this.playlistDetails = this.playlistApiService.getPlaylistById(playlistId);
+    this.playlistSongs = this.playlistApiService.getSongsByPlaylist(this.playlistDetails);
+    this.currentSong = undefined;
+    this.playlistDuration = getPlaylistDuration(this.playlistSongs);
+  }
 }
