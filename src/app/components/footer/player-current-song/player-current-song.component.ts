a6a37eddf0d55f405d2e09ea1ad914556ab30e72
Luis B <luis.beroiza@adesso.de>
2024-01-24 21:09:35 +0100

    feature show current song details ons audio track

--- src/app/components/footer/player-current-song/player-current-song.component.ts
+++ src/app/components/footer/player-current-song/player-current-song.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { songArtistAsString } from "@/libs/utitlities-song";
-import { songs } from "@/data/data";
+import { Song, songs } from "@/data/data";
+import { Store } from "@ngrx/store";
+import { AppState } from "@/store/app.state";
+import { SelectPlayerCurrentSong } from "@/store/player-store/playerstore.selectors";
 
 @Component({
   selector: 'player-current-song',
@@ -9,14 +12,29 @@ import { songs } from "@/data/data";
   templateUrl: './player-current-song.component.html',
   styleUrl: './player-current-song.component.css'
 })
-export class PlayerCurrentSongComponent {
+export class PlayerCurrentSongComponent implements OnInit{
 
   // Current song should be read from PlayerState
-  song = songs[0];
+  protected song: Song | undefined = undefined;
 
-  getArtists(): string {
-    return songArtistAsString(this.song.artists);
+
+  constructor(
+    private store: Store<AppState>
+  ){
   }
+
+  ngOnInit(): void {
+    this.store.select(SelectPlayerCurrentSong)
+      .subscribe(currentSong => {
+        this.song = currentSong;
+      });
+  }
+
+  protected getArtists(): string {
+    return songArtistAsString(this.song?.artists || []);
+  }
+
+
 
 
 }
