cd85548a4cbb8e89c92ca8e81146a176e0db4fec
Luis B <luis.beroiza@adesso.de>
2024-01-26 17:52:38 +0100

    feature show in green the name of the current played playlist on aside menu

--- src/app/components/aside/playlist-item/playlist-item.component.ts
+++ src/app/components/aside/playlist-item/playlist-item.component.ts
@@ -1,21 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
+import { Component, Input, OnInit } from '@angular/core';
+import type { AppState } from "@/store/app.state";
+import type { Playlist } from "@/data/data";
 import { RouterLink } from "@angular/router";
+import { SelectPlayerCurrentSong } from "@/store/player-store/playerstore.selectors";
+import { Store } from "@ngrx/store";
 import { colors } from "@/data/colors";
-import { type Playlist } from "@/data/data";
 import { songArtistAsString } from "@/libs/utitlities-song";
 
 @Component({
   selector: 'playlist-item',
   standalone: true,
   imports: [
-    NgOptimizedImage,
     RouterLink
   ],
   templateUrl: './playlist-item.component.html',
   styleUrl: './playlist-item.component.css'
 })
-export class PlaylistItemComponent {
+export class PlaylistItemComponent implements OnInit {
   @Input() playlistItem: Playlist = {
     id: '0',
     albumId: 0,
@@ -25,7 +26,20 @@ export class PlaylistItemComponent {
     artists: [],
   };
 
-  getArtists(): string {
+  protected isPlaylistPlaying: boolean = false;
+
+  constructor(private store: Store<AppState>) {
+  }
+
+
+  ngOnInit(): void {
+    this.store.select(SelectPlayerCurrentSong).subscribe(currentSong => {
+      this.isPlaylistPlaying = currentSong?.albumId === this.playlistItem.albumId;
+    });
+  }
+
+  protected getArtists(): string {
     return songArtistAsString(this.playlistItem.artists);
   }
+
 }
