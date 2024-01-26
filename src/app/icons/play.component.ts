ccdfdadc8d35a6f03f718645994187456dfba370
Luis B <luis.beroiza@adesso.de>
2024-01-26 20:28:27 +0100

    fix the icon svg use the fill color of the parent component

--- src/app/icons/play.component.ts
+++ src/app/icons/play.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <svg [class]="class" viewBox="0 0 16 16" role="img" height="16" width="16" >
+    <svg [class]="class" viewBox="0 0 16 16" role="img" height="16" width="16" fill="currentColor">
       <path
         d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
     </svg>
