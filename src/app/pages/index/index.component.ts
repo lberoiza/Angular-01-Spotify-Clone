b76046fc6160173bcd90da22d3afc4dc4b3c3677
Luis B <luis.beroiza@adesso.de>
2024-01-29 15:29:11 +0100

    Add Loading Component to index page
    Index page must load the playlist of the user.
    meanwhile should be displayed a loading component

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,18 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { Playlist } from "@/data/data";
 import { GreetingsUserComponent } from "@/components/main/greetings-user/greetings-user.component";
-import { Playlist, playlists } from "@/data/data";
 import { PlaylistCardComponent } from "@/components/main/playlist-card/playlist-card.component";
+import { ApplicationApiMock } from "@/service/ApplicationApiMock";
+import {
+  LoadingPlaylistCardComponent
+} from "@/components/common/loading-components/loading-playlist-card/loading-playlist-card.component";
 
 @Component({
   selector: 'index',
   standalone: true,
   imports: [
     GreetingsUserComponent,
+    LoadingPlaylistCardComponent,
     PlaylistCardComponent
   ],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrl: './index.component.css',
+  providers: [
+    ApplicationApiMock
+  ]
 })
-export class IndexComponent {
-  @Input() playlists: Playlist[] = playlists
+export class IndexComponent implements OnInit {
+
+  protected loading: boolean = false;
+  protected playlists: Playlist[] = [];
+
+  constructor(private applicationApi: ApplicationApiMock) {
+  }
+
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.applicationApi.getAllPlaylists()
+      .subscribe(
+        (playlists: Playlist[]) => {
+          this.playlists = playlists;
+          this.loading = false;
+        }
+      );
+  }
+
+
 }
