afaa1a18541e6303f5258db09f95ec8bb8781c67
Luis B <luis.beroiza@adesso.de>
2024-01-31 23:22:46 +0100

    Feature previous song of the list
    feature to play the previous song of the list

--- src/app/components/footer/player-buttons/player-button-back/player-button-back.component.ts
+++ src/app/components/footer/player-buttons/player-button-back/player-button-back.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { AppState } from "@/store/app.state";
+import type { Song } from "@/data/data";
 import { IconPlayerBackComponent } from "@/icons/player-back.component";
+import { PlayerState, RepeatType } from "@/models/state/playerstate.model";
+import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
+import { SelectPlayerState } from "@/store/player-store/playerstore.selectors";
+import { Store } from "@ngrx/store";
+import { previousSongInList } from "@/libs/utilities-song";
 
 @Component({
   selector: 'player-button-back',
@@ -10,9 +17,47 @@ import { IconPlayerBackComponent } from "@/icons/player-back.component";
   templateUrl: './player-button-back.component.html',
   styleUrl: './player-button-back.component.css'
 })
-export class PlayerButtonBackComponent {
+export class PlayerButtonBackComponent implements OnInit {
 
-  buttonPlayerBackClicked() {
-    console.log('button player back clicked');
+  protected playerState!: PlayerState;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
   }
+
+
+  ngOnInit(): void {
+    this.store
+      .select(SelectPlayerState)
+      .subscribe((playerState: PlayerState) => this.playerState = playerState);
+  }
+
+
+  protected buttonPlayerBackClicked(): void {
+    const currentSong: Song | undefined = this.playerState.currentMusic.song;
+
+    if (currentSong) {
+      this.store.dispatch(PlayerStoreActions.setIsPlaying({isPlaying: false}));
+      this.shouldStartSongOver() ? this.startSongOver() : this.loadPreviousSong(currentSong);
+      this.store.dispatch(PlayerStoreActions.setIsPlaying({isPlaying: true}));
+    }
+  }
+
+  private shouldStartSongOver(): boolean {
+    return this.playerState.currentTime > 2;
+  }
+
+  private startSongOver(): void {
+    this.store.dispatch(PlayerStoreActions.setCurrentTime({currentTime: 0}));
+  }
+
+  private loadPreviousSong(currentSong: Song): void {
+    const currentSongList: Song[] = this.playerState.currentMusic.songs;
+    const repeatPlaylist: boolean = this.playerState.repeatType === RepeatType.REPEAT_PLAYLIST;
+    const previousSong: Song = previousSongInList(currentSongList, currentSong, repeatPlaylist);
+    this.store.dispatch(PlayerStoreActions.setCurrentSong({song: previousSong}));
+  }
+
+
 }
