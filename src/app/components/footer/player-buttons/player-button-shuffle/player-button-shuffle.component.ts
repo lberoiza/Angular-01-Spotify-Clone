f201c7d8fe30e61dde1cee5c8e2b9a96fd26a896
Luis B <luis.beroiza@adesso.de>
2024-01-31 21:27:10 +0100

    Feature shuffle button shuffle the current song list of the store

--- src/app/components/footer/player-buttons/player-button-shuffle/player-button-shuffle.component.ts
+++ src/app/components/footer/player-buttons/player-button-shuffle/player-button-shuffle.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { AppState } from "@/store/app.state";
+import type { Song } from "@/data/data";
 import { IconPlayerControlShuffleComponent } from "@/icons/player-control-shuffle.component";
+import { PlayerStoreActions } from "@/store/player-store/playerstore.actions";
+import { Store } from "@ngrx/store";
+import { shuffleSongArray, sortSongArray } from "@/libs/utilities-song";
+import {
+  SelectPlayerCurrentPlaylistSongs
+} from "@/store/player-store/playerstore.selectors";
+
 
 @Component({
   selector: 'player-button-shuffle',
@@ -10,12 +19,37 @@ import { IconPlayerControlShuffleComponent } from "@/icons/player-control-shuffl
   templateUrl: './player-button-shuffle.component.html',
   styleUrl: './player-button-shuffle.component.css'
 })
-export class PlayerButtonShuffleComponent {
+export class PlayerButtonShuffleComponent implements OnInit {
 
   protected isShuffle: boolean = false;
+  protected currentSongs: Song[] = [];
+
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.store.select(SelectPlayerCurrentPlaylistSongs)
+      .subscribe((currentSongs: Song[] | undefined) => {
+          if (currentSongs) {
+            this.currentSongs = currentSongs;
+          }
+        }
+      );
+  }
 
   protected buttonPlayerControlShuffleClicked(): void {
-    console.log('button player control shuffle clicked');
     this.isShuffle = !this.isShuffle;
+    this.isShuffle ? this.shuffleCurrentSongs() : this.sortCurrentSongs();
+  }
+
+  private shuffleCurrentSongs() {
+    this.store.dispatch(PlayerStoreActions.setCurrentPlaylistSongs({songs: shuffleSongArray(this.currentSongs)}));
+  }
+
+  private sortCurrentSongs() {
+    this.store.dispatch(PlayerStoreActions.setCurrentPlaylistSongs({songs: sortSongArray(this.currentSongs)}));
   }
 }
