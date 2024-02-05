dfc57c2a6057c89196f51c16e80c2187259579f2
Luis B <luis.beroiza@adesso.de>
2024-02-05 10:50:35 +0100

    Automatic binding of Listeners for components who extends from clickable-holdable-component

--- src/app/libs/clickable-holdable-component.ts
+++ src/app/libs/clickable-holdable-component.ts
@@ -1,3 +1,6 @@
+import { Component, HostListener } from "@angular/core";
+
+@Component({ template: '' })
 export abstract class ClickableHoldableComponent {
 
   protected considerHoldingAfterMillisecond: number = 500;
@@ -5,11 +8,11 @@ export abstract class ClickableHoldableComponent {
   protected isComponentPressed: boolean = false;
 
 
-
   protected abstract executeByClick(): void;
   protected abstract executeWhilePressing(): void;
 
 
+  @HostListener('mousedown')
   protected pressButton = () => {
     this.intervalId = setInterval(() => {
       this.isComponentPressed = true;
@@ -17,6 +20,7 @@ export abstract class ClickableHoldableComponent {
     }, this.considerHoldingAfterMillisecond);
   }
 
+  @HostListener('mouseup')
   protected releaseButton = () => {
     if (!this.isComponentPressed) {
       this.executeByClick();
@@ -24,6 +28,7 @@ export abstract class ClickableHoldableComponent {
     this.leaveButton();
   }
 
+  @HostListener('mouseleave', ['$event'])
   protected leaveButton = () => {
     clearInterval(this.intervalId);
     this.isComponentPressed = false;
