33391d7c19dca25c09aa52678b66c1c21fd45f4e
Luis B <luis.beroiza@adesso.de>
2024-02-06 13:17:55 +0100

    Add NGRX Effects to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { routes } from './app.routes';
+
 import { ROOT_REDUCERS } from "@/store/app.state";
 
 export const appConfig: ApplicationConfig = {
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions()),
     provideAnimations(),
     provideStore(ROOT_REDUCERS),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
 ]
 };
