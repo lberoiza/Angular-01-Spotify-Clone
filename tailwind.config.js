633aeb7d88cd142e4e50e97e1fc5852bec46ae30
Luis B <luis.beroiza@adesso.de>
2024-02-13 17:47:26 +0100

    add new tailwind breakpoint for media queries 'xs' => '550px'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '550px',
+      },
+    },
   },
   plugins: [],
 }
