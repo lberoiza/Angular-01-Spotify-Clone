c4a428d2a777adaee532848e0d6bd87e0b6ebf73
Luis B <luis.beroiza@adesso.de>
2024-02-17 23:21:16 +0100

    feature the App can now play music from another sites
    Now the app can play music saved locally or from the internet

--- src/app/libs/assets.ts
+++ src/app/libs/assets.ts
@@ -3,8 +3,7 @@ import type { Playlist, Song } from "@/data/data";
 export const MUSIC_DIRECTORY = 'assets/music';
 
 export function getSongUrl(song: Song | undefined): string {
-  if (!song) {
-    return '';
-  }
-  return `${MUSIC_DIRECTORY}/${song?.albumId}/${song?.filename}`
+  if (!song) return '';
+  if (song.isStored) return `${MUSIC_DIRECTORY}/${song?.albumId}/${song?.url}`
+  return song.url
 }
