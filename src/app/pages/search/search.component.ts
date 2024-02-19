5490ec396d26c307173ae4082f4061230c7e8813
Luis B <luis.beroiza@adesso.de>
2024-02-19 22:55:24 +0100

    show in the view the result of the search

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { Song } from "@/data/data";
+import { ApplicationApiMock } from "@/service/ApplicationApiMock";
 import { SearchIconComponent } from "@/icons/search-icon.component";
+import { debounceTime, Subject, take } from "rxjs";
+import {
+  PlaylistDetailsMusictableComponent
+} from "@/components/main/playlist-details-musictable/playlist-details-musictable.component";
+
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
-    SearchIconComponent
+    SearchIconComponent,
+    PlaylistDetailsMusictableComponent
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  protected results: Song[] = [];
+  protected searchSubject = new Subject<string>();
+
+  constructor(private applicationApi: ApplicationApiMock) {
+  }
+
+  ngOnInit() {
+    this.searchSubject.pipe(
+      debounceTime(300)
+    ).subscribe(searchValue => {
+      if (searchValue.length < 3) return;
+
+      this.applicationApi.getSongsBySearchString(searchValue)
+        .pipe(take(1))
+        .subscribe(songs => {
+          this.results = songs;
+        });
+    });
+  }
+
+
+  protected onSearchInputKeyUp(event: KeyboardEvent) {
+    const searchValue = (event.target as HTMLInputElement).value;
+    if (searchValue) {
+      this.searchSubject.next(searchValue);
+    } else {
+      this.results = [];
+    }
+  }
 
 }
