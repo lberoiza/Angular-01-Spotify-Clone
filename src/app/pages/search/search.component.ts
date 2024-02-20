b096dc5eb17659d6dfd5a6158a66afcfb697e7f4
Luis B <luis.beroiza@adesso.de>
2024-02-20 22:41:16 +0100

    Use Search effect to get information from api
    This methodology decouples the API calls from the component logic

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,13 +1,17 @@
-import { ApplicationApiMock } from "@/service/ApplicationApiMock";
+import { AppState } from "@/store/app.state";
 import { Component, OnInit } from '@angular/core';
+import { PlaylistCardComponent } from "@/components/main/playlist-card/playlist-card.component";
 import { SearchByStringType } from "@/service/IApplicationAPI";
 import { SearchIconComponent } from "@/icons/search-icon.component";
-import { debounceTime, Subject, take } from "rxjs";
-import { PlaylistCardComponent } from "@/components/main/playlist-card/playlist-card.component";
+import { Store } from "@ngrx/store";
+import { debounceTime, Subject } from "rxjs";
 
 import {
   PlaylistDetailsMusictableComponent
 } from "@/components/main/playlist-details-musictable/playlist-details-musictable.component";
+import { SearchStoreActions } from "@/store/search-store/searchstore.actions";
+import { SelectSearchIsSearching, SelectSearchLastResult } from "@/store/search-store/searchstore.selectors";
+import { SearchResult } from "@/models/state/searchstate.model";
 
 
 @Component({
@@ -23,41 +27,43 @@ import {
 })
 export class SearchComponent implements OnInit {
 
-  protected results!: SearchByStringType;
+  protected results!: SearchResult;
   protected searchSubject = new Subject<string>();
+  protected isSearching: boolean = false;
 
-  constructor(private applicationApi: ApplicationApiMock) {
+  constructor(private store: Store<AppState>) {
     this.initializeSearch();
   }
 
   ngOnInit() {
+    this.initializeSearchSubject();
+    this.store.select(SelectSearchIsSearching).subscribe(isSearching => this.isSearching = isSearching);
+    this.store.select(SelectSearchLastResult).subscribe(lastResult => this.results = lastResult);
+  }
+
+  private initializeSearchSubject() {
     this.searchSubject.pipe(
       debounceTime(300)
     ).subscribe(searchValue => {
       if (searchValue.length < 3) return;
-
-      this.applicationApi.getPlaylistAndSongsBySearchString(searchValue)
-        .pipe(take(1))
-        .subscribe(result => {
-          this.results = result;
-        });
+      this.store.dispatch(SearchStoreActions.search({searchString: searchValue}));
     });
-  }
-
-
-  protected onSearchInputKeyUp(event: Event) {
-    const searchValue = (event.target as HTMLInputElement).value;
-    if (searchValue) {
-      this.searchSubject.next(searchValue);
-    } else {
-      this.initializeSearch();
-    }
   }
 
   private initializeSearch() {
     this.results = {
+      searchString: '',
       playlists: [],
       songs: []
+    }
+  }
+
+  protected onSearchInput(event: Event) {
+    const searchValue = (event.target as HTMLInputElement).value;
+    if (searchValue) {
+      this.searchSubject.next(searchValue);
+    } else {
+      this.store.dispatch(SearchStoreActions.setResult({searchString: '', playlists: [], songs: []}));
     }
   }
 
