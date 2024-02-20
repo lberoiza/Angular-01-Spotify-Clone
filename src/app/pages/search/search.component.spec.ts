27071620c1cb208041739a4cec127fb322efbbc8
Luis B <luis.beroiza@adesso.de>
2024-02-20 22:59:57 +0100

    fix test by adding providers: [provideMockStore()]

--- src/app/pages/search/search.component.spec.ts
+++ src/app/pages/search/search.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { provideMockStore } from "@ngrx/store/testing";
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,10 +9,11 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
+      imports: [SearchComponent],
+      providers: [provideMockStore()]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
